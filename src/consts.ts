3a0b7f1721ffb6ba3d3302638aa59baacc808006
JanuDev <93274170+KizaKonpaku@users.noreply.github.com>
2024-03-02 13:06:52 -0400

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,97 @@
+// To Do: Add svg icons of my proyects
+
+export const SITE_TITLE = "JanuDev"
+export const SITE_DESCRIPTION = "Welcome to my website!";
+
+export const stack = [
+  {
+    name: "HTML",
+    color: "#EF652A",
+  },
+  {
+    name: "CSS",
+    color: "#30A9DC",
+  },
+  {
+    name: "JavaScript",
+    color: "#F0DB4F",
+  },
+  {
+    name: "TypeScript",
+    color: "#3178C6",
+  },
+  {
+    name: "Tailwind",
+    color: "#17BAB8",
+  },
+  {
+    name: "React",
+    color: "#00D8FF",
+  },
+  {
+    name: "Redux",
+    color: "#764ABC",
+  },
+  {
+    name: "Next",
+    color: "#FAFAFA",
+  },
+  {
+    name: "Astro",
+    color: "#FAFAFA",
+  },
+  {
+    name: "NodeJS",
+    color: "#539E43",
+  },
+  {
+    name: "ExpressJS",
+    color: "#FAFAFA",
+  },
+  {
+    name: "GraphQL",
+    color: "#F6009C",
+  },
+  {
+    name: "Testing Library",
+    color: "#DC312F",
+  },
+  {
+    name: "Git",
+    color: "#DE4C36",
+  },
+  {
+    name: "VS Code",
+    color: "#3DA9F2",
+  },
+];
+export const projects = [
+  {
+    name: "Web design Illustrator",
+    description:
+      '',
+   //  image: honoLogo,
+    url: "/",
+  },
+  {
+    name: "Gallery",
+    description:
+      '',
+    // image: berlinLogo,
+     url: "/",
+  },
+  {
+    name: "Hollow Knight Landing page",
+    description:
+      '',
+    // image: paletaLogo, IMAGEN
+     url: "/",
+  },
+  {
+    name: "Clone Spotify",
+    description:
+      '',
+    // image: colorsKitLogo,
+     url: "/",
+  },
+];
