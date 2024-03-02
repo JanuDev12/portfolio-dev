f8495f53db81874272972b51da913e85e2f506a9
JanuDev <93274170+KizaKonpaku@users.noreply.github.com>
2024-03-02 13:06:52 -0400

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,97 @@
+// To Do: Add svg icons of my proyects
+
+export const SITE_TITLE = "JanuDev"
+export const SITE_DESCRIPTION = "Welcome to my website!";
+
+export const stack = [
+  {
+    name: "HTML",
+    color: "#EF652A",
+  },
+  {
+    name: "CSS",
+    color: "#30A9DC",
+  },
+  {
+    name: "JavaScript",
+    color: "#F0DB4F",
+  },
+  {
+    name: "TypeScript",
+    color: "#3178C6",
+  },
+  {
+    name: "Tailwind",
+    color: "#17BAB8",
+  },
+  {
+    name: "React",
+    color: "#00D8FF",
+  },
+  {
+    name: "Redux",
+    color: "#764ABC",
+  },
+  {
+    name: "Next",
+    color: "#FAFAFA",
+  },
+  {
+    name: "Astro",
+    color: "#FAFAFA",
+  },
+  {
+    name: "NodeJS",
+    color: "#539E43",
+  },
+  {
+    name: "ExpressJS",
+    color: "#FAFAFA",
+  },
+  {
+    name: "GraphQL",
+    color: "#F6009C",
+  },
+  {
+    name: "Testing Library",
+    color: "#DC312F",
+  },
+  {
+    name: "Git",
+    color: "#DE4C36",
+  },
+  {
+    name: "VS Code",
+    color: "#3DA9F2",
+  },
+];
+export const projects = [
+  {
+    name: "Web design Illustrator",
+    description:
+      'A light, simple and easy to use third party <b style="color: #FAA628;">Hono</b> middleware for <b style="color: #FAA628;">OAuth 2.0</b> authentication with providers like Facebook, Github, Google, X, etc. Part of the <b style="color: #FAA628;">official</b> Hono middlewares.',
+   //  image: honoLogo,
+    url: "/",
+  },
+  {
+    name: "Gallery",
+    description:
+      'AI generative web app that converts <b style="color: #FAA628;">Images to code</b>, offering a freemium subscription that provides 8 free credits per month to every user. The code generation is made by ChatGPT using the OpenAI API.',
+    // image: berlinLogo,
+     url: "/",
+  },
+  {
+    name: "Hollow Knight Landing page",
+    description:
+      'A <b style="color: #FAA628;">Web App</b> to craft stunning color palettes, simulate color blindness for accessibility, create eye-catching gradients and animations, and save your favorites for easy access. Plus, add a personal touch with font icon creation.',
+    // image: paletaLogo, IMAGEN
+     url: "/",
+  },
+  {
+    name: "Clone Spotify",
+    description:
+      '<b style="color: #FAA628;">Npm package</b> for color manipulation in formats like CMYK, RGB, etc. With it, you can convert a color between formats, simulate color blindness, generate all sorts of palettes, image color extraction, and more.',
+    // image: colorsKitLogo,
+     url: "/",
+  },
+];
