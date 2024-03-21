3778421101bc26030cbbad1bae7bf1836db32289
JanuDev <93274170+KizaKonpaku@users.noreply.github.com>
2024-03-20 21:02:40 -0400

    deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+import vercelStatic from "@astrojs/vercel/static";
+
+export default defineConfig({
+  output: "static",
+  adapter: vercelStatic(),
+});
